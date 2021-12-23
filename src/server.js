f371b774b92d3dabd490cf2757fe720314bb1d37
eos0103 <sola2014@naver.com>
2021-12-24 00:44:21 +0900

    #1.3

--- src/server.js
+++ src/server.js
@@ -10,9 +10,15 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log('Listening on http://localhost');
+const handleListen = () => console.log('Listening on http://localhost:3000');
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection);
 
 server.listen(3000, handleListen);
